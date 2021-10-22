8e88911ebe0a3baf396278f2c698ebc29c45898d
Berezin.AA1 <berezin.aa1@dns-shop.ru>
2021-10-22 17:50:41 +1000

    Fix Task model import in test

--- task_manager/apps/tasks/tests.py
+++ task_manager/apps/tasks/tests.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 from task_manager.apps.statuses.models import Status
-from task_manager.models import Task
+from task_manager.apps.tasks.models import Task
 
 LOGIN_SANSA = (reverse_lazy('login'), {'username': 'SansaStark', 'password': 'aaa12345'})
 
